da29f0f8902c1d902efcfdf1bd0ffe7a13ae8f81
Valerij Meleschenko <kingstonchet@gmail.com>
2018-07-28 21:38:21 +0300

    tree: Rename dtb* to dt*
    Signed-off-by: Valerij Meleschenko <kingstonchet@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j1x3g/dtb.img
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j1x3g/dt.img
 
 
 #TARGET_KERNEL_SOURCE := kernel/samsung/j1x3g
